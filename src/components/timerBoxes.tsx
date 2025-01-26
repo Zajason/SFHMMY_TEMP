9e75be28c61336a8da2ab1a9252f9a0f591ec291
Chrysostomos Koumides <chyrkoum@gmail.com>
2025-01-26 19:35:06 +0200

    refactor layout and styles for TimerBoxes and index page; added buttons for Call for Papers and PreΣΦΗΜΜΥ

--- src/components/timerBoxes.tsx
+++ src/components/timerBoxes.tsx
@@ -36,11 +36,11 @@ const TimerBoxes: React.FC<TimerBoxesProps> = ({ endTime }) => {
   }, []);
 
   return (
-    <div className="flex space-x-4 mt-2">
+    <div className="flex space-x-2 mt-2">
       {Object.values(timeLeft).map((value, index) => (
         <div
           key={index}
-          className="flex items-center justify-center bg-[#737AB5] text-gray-100 font-bold w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 text-2xl sm:text-3xl md:text-6xl rounded-lg shadow-lg"
+          className="flex items-center justify-center bg-[#737AB5] text-gray-100 font-bold w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 text-lg sm:text-xl md:text-2xl rounded-lg shadow-lg"
         >
           {value.toString().padStart(2, "0")}
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Meteors } from "../components/meteorAnimation.tsx";
-import TimerBoxes from "../components/timerBoxes"; // New TimerBoxes component
+import TimerBoxes from "../components/timerBoxes";
 
 const IndexPage: React.FC = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <div className="relative bg-gray-900 h-screen w-screen flex flex-col items-center justify-center text-white overflow-hidden">
       <Meteors />
-      <div className="flex flex-col items-center -mt-40">
+      <div className="flex flex-col items-center -mt-20">
         <img
           src="/images/Official Logo ΣΦΗΜΜΥ 16 for dark.png"
           alt="ΣΦΗΜΜΥ Logo"
-          className="h-[400] sm:h-[400] md:h-[600px] lg:h-[700px] xl:h-[700] mb-4"
+          className="h-96 lg:h-[400px] mb-4"
         />
-
-        <h1 className="text-4xl mb-4">We are coming soon</h1>
+        <h1 className="text-2xl sm:text-3xl md:text-4xl mb-4">
+          We are coming soon
+        </h1>
         <TimerBoxes endTime="2025-04-25" />
+        <div className="mt-8 flex gap-4">
+          {/* Call for Papers Button */}
+          <a
+            href="https://papers.sfhmmy.gr/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-6 py-3 text-sm sm:text-base md:text-lg font-semibold rounded-lg shadow-md bg-[#7486cb] hover:bg-[#6778b7] transition-all duration-300 transform hover:-translate-y-1 hover:scale-105"
+          >
+            Call for Papers
+          </a>
+          {/* PreΣΦΗΜΜΥ Button */}
+          <a
+            href="https://pre-sfhmmy.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-6 py-3 text-sm sm:text-base md:text-lg font-semibold rounded-lg shadow-md bg-[#77bb4e] hover:bg-[#6aa944] transition-all duration-300 transform hover:-translate-y-1 hover:scale-105"
+          >
+            PreΣΦΗΜΜΥ
+          </a>
+        </div>
       </div>
     </div>
   );
